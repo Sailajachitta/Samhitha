ae32c27174aefb9d605b0b27aee798dbcb12fc91
sailajachitta <nvrsailaja@gmail.com>
2020-08-30 21:52:51 +0530

    Committing sammyOneClass to the master

---
+++ sammy/src/com/sammy/demo/sammyOne.java
@@ -0,0 +1,10 @@
+package com.sammy.demo;
+
+public class sammyOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("added first print satement");
+	}
+
+}
