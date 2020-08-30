1030574fae2cc14b03781e8ded6fe621cb3b2661
sailajachitta <nvrsailaja@gmail.com>
2020-08-30 22:28:30 +0530

    committing sammysecond class

--- sammy/src/com/sammy/demo/sammyTwo.java
+++ sammy/src/com/sammy/demo/sammyTwo.java
@@ -4,7 +4,7 @@ public class sammyTwo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("added second sammy class");
+		System.out.println("added second sammy class ");
 
 	}
 
